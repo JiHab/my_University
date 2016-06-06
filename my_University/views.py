c94b8de9448b1cb7cde42d01fdd3fb6dd2702bee
jihab <vladimir.mail@ukr.net>
2016-06-06 23:28:35 +0300

    Added h1 tag

--- my_University/views.py
+++ my_University/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib import auth
 from my_University.models import Group, Student
 
 
 def home(request):
     groups = Group.objects.all()
     students = Student.objects.all()
-    print(students)
+    print(auth.get_user(request))
     context = {
         'groups': groups,
-        'students': students
+        'students': students,
+        'username': auth.get_user(request)
     }
-
+    print()
     return render(request, 'home.html', context)
